2ba2334e248d987a5f859195205016dfaf24a552
chenlelin <chenlelin@kylinos.cn>
2021-03-27 01:28:07 +0800

    Fix bug can not show auth-window when connect a portal wifi

--- src/wifi-auth-thread.cpp
+++ src/wifi-auth-thread.cpp
@@ -1,14 +1,20 @@
 #include "wifi-auth-thread.h"
+
+#include <QDebug>
+
 void WifiAuthThread::run()
 {
+    sleep(3);
 	QDBusInterface interface( "org.freedesktop.NetworkManager",
 			"/org/freedesktop/NetworkManager",
 			"org.freedesktop.NetworkManager",
 			QDBusConnection::systemBus() );
 	QDBusReply<uint> reply= interface.call("CheckConnectivity");
 	if (reply.isValid()) {
-		if (reply.value() == 2)
-			system("gdbus call --session --dest org.gnome.Shell.PortalHelper --object-path /org/gnome/Shell/PortalHelper --method org.gtk.Application.Activate []");
+        //qDebug() << "-----------------------> 000000000000000011" << reply.value();
+        if (reply.value() == 2) {
+            system("gdbus call --session --dest org.gnome.Shell.PortalHelper --object-path /org/gnome/Shell/PortalHelper --method org.gtk.Application.Activate []");
+        }
 	}
 }
 

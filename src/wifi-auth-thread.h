cbb9bb071772d65c6f3a3e46cfe5fa1b0a29c59e
chenlelin <chenlelin@kylinos.cn>
2021-03-27 21:00:40 +0800

    Optmize connect portal wifi function

--- src/wifi-auth-thread.h
+++ src/wifi-auth-thread.h
@@ -1,6 +1,10 @@
 #ifndef WIFI_AUTH_THREAD_H
 #define WIFI_AUTH_THREAD_H
 #include <QThread>
+#include <QNetworkAccessManager>
+#include <QNetworkReply>
+#include <QNetworkRequest>
+#include <QProcess>
 #include <QDBusInterface>
 #include <QDBusReply>
 
@@ -8,6 +12,14 @@ class WifiAuthThread:public QThread
 {
 protected:
     virtual void run();
+    QNetworkRequest request;
+    QNetworkAccessManager* naManager=nullptr;
+    bool _isConnect=true;//是否已联网
+    bool _isReply=false;//联网状态检查是否收到回复
+
+public:
+    WifiAuthThread();
+    ~WifiAuthThread();
 };
 
 #endif //WIFI_AUTH_THREAD_H

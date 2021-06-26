7de77f2551f32690e240b2143dec18b4897dd55e
chenlelin <chenlelin@kylinos.cn>
2021-06-26 14:58:51 +0800

    Fix bug-57502 show wrong bluetooth information in wired user interface

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1785,7 +1785,7 @@ void MainWindow::getLanListDone(QStringList slist)
         bool isActiveNet = false; //isActiveNet用来表明nname是否是活动的连接
 
         //仅仅对有线网络进行添加列表处理
-        if (ltype != "802-11-wireless" && ltype != "wifi" && ltype != "bridge" && ltype != "" && ltype != "--") {
+        if (ltype != "802-11-wireless" && ltype != "wifi" && ltype != "bridge" && ltype != "bluetooth" && ltype != "" && ltype != "--") {
             objKyDBus->getLanIpDNS(nuuid, true); //使用UUID获取有线网的ip和dns信息
             QString macLan = getMacByUuid(nuuid); //有线网对应的mac地址
 

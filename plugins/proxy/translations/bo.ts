8c357e15df4a9c5607af5a1e8e42d69c1a7cc740
zhangyuanyuan1 <zhangyuanyuan@kylinos.cn>
2022-09-24 17:08:28 +0800

    应用代理界面

---
+++ plugins/proxy/translations/bo.ts
@@ -0,0 +1,229 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="bo_CN">
+<context>
+    <name>AptProxyDialog</name>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="24"/>
+        <source>Set Apt Proxy</source>
+        <translation>APTལས་ཚབ་གསར་འཛུགས་།</translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="41"/>
+        <source>Server Address</source>
+        <translation>ཞབས་ཞུ་ཆས་ཤག་གནས་</translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="59"/>
+        <source>Port</source>
+        <translation>མཐུད་ཁ་</translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="80"/>
+        <source>Cancel</source>
+        <translation>མེད་པར་བཟོ་བ་</translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="84"/>
+        <source>Confirm</source>
+        <translation>གཏན་ཁེལ་</translation>
+    </message>
+</context>
+<context>
+    <name>Proxy</name>
+    <message>
+        <location filename="../proxy.cpp" line="55"/>
+        <source>Proxy</source>
+        <translation>ལས་ཚབ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="185"/>
+        <source>Start using</source>
+        <translation>སྤྱོད་པ།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="198"/>
+        <source>Proxy mode</source>
+        <translation>ལས་ཚབ་བྱེད་སྟངས་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="202"/>
+        <source>Auto</source>
+        <translation>རང་འགུལ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="205"/>
+        <source>Manual</source>
+        <translation>ལག་སྒུལ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="360"/>
+        <source>Application Proxy</source>
+        <translation>ཉེར་སྤྱོད་ལས་ཚབ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="435"/>
+        <source>System Proxy</source>
+        <translation>རྒྱུད་ཁོངས་ལས་ཚབ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="437"/>
+        <source>Auto url</source>
+        <translation>བཀོད་སྒྲིག་URL</translation>
+        <extra-contents_path>/Proxy/Auto url</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="439"/>
+        <source>Http Proxy</source>
+        <translation>HTTP ལས་ཚབ་</translation>
+        <extra-contents_path>/Proxy/Http Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="441"/>
+        <source>Https Proxy</source>
+        <translation>HTTPS ལས་ཚབ་</translation>
+        <extra-contents_path>/Proxy/Https Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="443"/>
+        <source>Ftp Proxy</source>
+        <translation>FTP ལས་ཚབ་</translation>
+        <extra-contents_path>/Proxy/Ftp Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="445"/>
+        <source>Socks Proxy</source>
+        <translation>SOCKS ལས་ཚབ་</translation>
+        <extra-contents_path>/Proxy/Socks Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="446"/>
+        <location filename="../proxy.cpp" line="447"/>
+        <location filename="../proxy.cpp" line="448"/>
+        <location filename="../proxy.cpp" line="449"/>
+        <location filename="../proxy.cpp" line="934"/>
+        <source>Port</source>
+        <translation>མཐུད་ཁ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="450"/>
+        <source>List of ignored hosts. more than one entry, please separate with english semicolon(;)</source>
+        <translation>སྣང་མེད་དུ་བཞག་པའི་གཙོ་ཆས་རེའུ་མིག  དབྱིན་ཡིག་གི་འབྱེད་རྟགས།（;）</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="453"/>
+        <source>Apt Proxy</source>
+        <translation>APT ལས་ཚབ་</translation>
+        <extra-contents_path>/Proxy/Apt Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="454"/>
+        <source>Open</source>
+        <translation>ཁ་འབྱེད་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="455"/>
+        <source>Server Address : </source>
+        <translation>ཞབས་ཞུ་ཆས་ཤག་གནས་: </translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="456"/>
+        <source>Port : </source>
+        <translation>མཐུད་ཁ་： </translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="457"/>
+        <source>Edit</source>
+        <translation>རྩོམ་སྒྲིག</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="555"/>
+        <source>The apt proxy  has been turned off and needs to be restarted to take effect</source>
+        <translation>APTལས་ཚབ་སྒོ་རྒྱག་དང་།བསྐྱར་སྒུལ་བྱས་ན་གཞི་ནས་ནུས་པ་ཐོན་།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="556"/>
+        <location filename="../proxy.cpp" line="777"/>
+        <source>Reboot Later</source>
+        <translation>ཅུང་ཙམ་འགོར་རྗེས་བསྐྱར་སྒུལ་།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="557"/>
+        <location filename="../proxy.cpp" line="778"/>
+        <source>Reboot Now</source>
+        <translation>འཕྲལ་མར་བསྐྱར་འབྱེད་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="776"/>
+        <source>The system needs to be restarted to set the Apt proxy, whether to reboot</source>
+        <translation>APTལས་ཚབ་གསར་འཛུགས་མ་ལག་བསྐྱར་འབྱེད་རྗེས་ནུས་པ་ཐོན་།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="883"/>
+        <source>Proxy type</source>
+        <translation>ངོ་ཚབ་ཀྱི་རིགས།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="891"/>
+        <source>HTTP</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="892"/>
+        <source>socks4</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="893"/>
+        <source>socks5</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="900"/>
+        <source>IP address</source>
+        <translation>IP ཤག་གནས།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="906"/>
+        <location filename="../proxy.cpp" line="937"/>
+        <source>Required</source>
+        <translation>འབྲི་དགོས་ངེས་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="927"/>
+        <source>Invalid IP Address</source>
+        <translation>གོ་མི་ཆོད་པའི་IPས་གནས།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="948"/>
+        <source>Username</source>
+        <translation>སྤྱོད་མཁན་གྱི་མིང་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="951"/>
+        <location filename="../proxy.cpp" line="965"/>
+        <source>Optional</source>
+        <translation>འདེམ་སྐོང་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="961"/>
+        <source>Password</source>
+        <translation>གསང་ཨང་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="983"/>
+        <source>Cancel</source>
+        <translation>མེད་པར་བཟོ་བ་</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="984"/>
+        <source>Save</source>
+        <translation>ཉར་ཚགས།</translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1034"/>
+        <source>The following applications are allowed to use this configuration:</source>
+        <translation>གཤམ་གསལ་ལྟར་བཀོད་སྒྲིག་བྱས་ཆོག་པ་སྟེ།：</translation>
+    </message>
+</context>
+</TS>

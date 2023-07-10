633fbd4c1e2aeda152ca852a336d27284548e11b
zhangyuanyuan1 <zhangyuanyuan1@kylinos.cn>
2023-07-10 10:03:35 +0800

    多DNS复制不全问题

--- src/frontend/netdetails/detailpage.cpp
+++ src/frontend/netdetails/detailpage.cpp
@@ -349,7 +349,11 @@ void DetailPage::on_btnCopyNetDetail_clicked()
     bandwithCopy += this->m_BandWidth->text();
     ipv6Copy += m_formerIPV6;
     ipv4Copy += this->m_IPV4->text();
-    ipv4dnsCopy += this->m_IPV4Dns->text();
+    if (m_IPV4Dns->toolTip().isEmpty()) {
+        ipv4dnsCopy += this->m_IPV4Dns->text();
+    } else {
+        ipv4dnsCopy += this->m_IPV4Dns->toolTip();
+    }
     macCopy += this->m_Mac->text();
     netDetailList.append(bandwithCopy);
     netDetailList.append(ipv4Copy);

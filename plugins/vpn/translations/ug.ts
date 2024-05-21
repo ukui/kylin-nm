cd7b29b0183e11c133ba83cea23b89cb06c0a556
KevinDuan <duankaiwen@kylinos.com>
2024-05-21 07:38:40 +0000

    Translated using Weblate (Uyghur)
    Currently translated at 100.0% (12 of 12 strings)
    
    Translation: openkylin-nile-new/vpn
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-nile-new/vpn/ug/

--- plugins/vpn/translations/ug.ts
+++ plugins/vpn/translations/ug.ts
@@ -19,7 +19,7 @@
     <message>
         <location filename="../itemframe.cpp" line="38"/>
         <source>Add VPN</source>
-        <translation type="unfinished"></translation>
+        <translation>VPN قوشۇش</translation>
     </message>
 </context>
 <context>
@@ -39,26 +39,26 @@
         <location filename="../vpn.cpp" line="157"/>
         <location filename="../vpn.cpp" line="284"/>
         <source>Show on Taskbar</source>
-        <translation type="unfinished"></translation>
+        <translation>ۋەزىپە ستونىدا سىنبەلگە كۆرسىتىش</translation>
         <extra-contents_path>/Vpn/Show on Taskbar</extra-contents_path>
     </message>
     <message>
         <location filename="../vpn.cpp" line="286"/>
         <source>Add VPN</source>
-        <translation type="unfinished"></translation>
+        <translation>VPN قوشۇش</translation>
         <extra-contents_path>/Vpn/Add VPN</extra-contents_path>
     </message>
     <message>
         <location filename="../vpn.cpp" line="372"/>
         <location filename="../vpn.cpp" line="479"/>
         <source>connected</source>
-        <translation type="unfinished"></translation>
+        <translation>ئۇلانغان</translation>
     </message>
     <message>
         <location filename="../vpn.cpp" line="374"/>
         <location filename="../vpn.cpp" line="489"/>
         <source>not connected</source>
-        <translation type="unfinished"></translation>
+        <translation>ئۇلانمىغان</translation>
     </message>
 </context>
 <context>
@@ -66,19 +66,19 @@
     <message>
         <location filename="../vpnitem.cpp" line="55"/>
         <source>Delete</source>
-        <translation type="unfinished"></translation>
+        <translation>ئۆچۈرۈش</translation>
     </message>
     <message>
         <location filename="../vpnitem.cpp" line="107"/>
         <location filename="../vpnitem.cpp" line="120"/>
         <source>Disconnect</source>
-        <translation type="unfinished"></translation>
+        <translation>ئۈزۈۋېتىش</translation>
     </message>
     <message>
         <location filename="../vpnitem.cpp" line="109"/>
         <location filename="../vpnitem.cpp" line="118"/>
         <source>Connect</source>
-        <translation type="unfinished"></translation>
+        <translation>ئۇلىنىش</translation>
     </message>
 </context>
 </TS>

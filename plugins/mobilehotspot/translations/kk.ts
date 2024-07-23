00f4c58f447b3e2bcc9f304938f3daca18bafba6
KevinDuan <duankaiwen@kylinos.cn>
2024-07-23 13:00:41 +0000

    Translated using Weblate (Kazakh)
    Currently translated at 100.0% (22 of 22 strings)
    
    Translation: openkylin-nile-new/mobilehotspot
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-nile-new/mobilehotspot/kk/

--- plugins/mobilehotspot/translations/kk.ts
+++ plugins/mobilehotspot/translations/kk.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../blacklistitem.cpp" line="50"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>شىعارىپ وتۋ</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../blacklistpage.cpp" line="47"/>
         <source>Blacklist</source>
-        <translation type="unfinished"></translation>
+        <translation>ماتەريال قارا تٸزٸمدٸگٸ</translation>
     </message>
 </context>
 <context>
@@ -22,7 +22,7 @@
     <message>
         <location filename="../connectdevlistitem.cpp" line="50"/>
         <source>drag into blacklist</source>
-        <translation type="unfinished"></translation>
+        <translation>قارا تٸزٸمدٸككە قوسۋ</translation>
     </message>
 </context>
 <context>
@@ -30,7 +30,7 @@
     <message>
         <location filename="../connectdevpage.cpp" line="48"/>
         <source>Connect device</source>
-        <translation type="unfinished"></translation>
+        <translation>اسبابٸن اۋلاۋ</translation>
     </message>
 </context>
 <context>
@@ -38,18 +38,18 @@
     <message>
         <location filename="../mobilehotspot.cpp" line="35"/>
         <source>MobileHotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشەمەكشى اعٸم نۇكتەسى</translation>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="101"/>
         <source>mobilehotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشەمەكشى اعٸم نۇكتەسى</translation>
         <extra-contents_path>/mobilehotspot/mobilehotspot</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="103"/>
         <source>mobilehotspot open</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشەمەكشى اعٸم نۇكتەسى ٸشٸۋ</translation>
         <extra-contents_path>/mobilehotspot/mobilehotspot open</extra-contents_path>
     </message>
 </context>
@@ -66,7 +66,7 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="159"/>
         <source>wirless switch is close or no wireless device</source>
-        <translation type="unfinished"></translation>
+        <translation>سىمسىز ايىرپ قوسقىش جابٸلدٸ ياكي قىزىق نۇكتە رولدارٸ بار سىمسىز تور كارتاسى ساقتالعان ەمەس</translation>
     </message>
     <message>
         <source>start to close hotspot</source>
@@ -75,7 +75,7 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="180"/>
         <source>hotpots name or device is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>اعٸم نۇكتە مى ياكي اسباب كۇشكە يە ەمەس</translation>
     </message>
     <message>
         <source>can not  create hotspot with password length less than eight!</source>
@@ -88,17 +88,17 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="563"/>
         <source>Contains at least 8 characters</source>
-        <translation type="unfinished"></translation>
+        <translation>از دەگەندە 8 ٵربى ٶز ىشىنە الادٸ</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="233"/>
         <source>Hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>اعٸم نۇكتەسى</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="64"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>تەڭشەۋلەر</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="67"/>
@@ -119,38 +119,38 @@
         <location filename="../mobilehotspotwidget.cpp" line="354"/>
         <location filename="../mobilehotspotwidget.cpp" line="712"/>
         <source>hotspot already close</source>
-        <translation type="unfinished"></translation>
+        <translation>اعٸم نۇكتەسى الدەقاشان جابىلىپ قالدى</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="509"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>ٸشٸۋ</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="530"/>
         <source>Wi-Fi Name</source>
-        <translation type="unfinished"></translation>
+        <translation>سىمسىز تور مى</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="551"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>قۇپيا نومەر</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="595"/>
         <source>Frequency band</source>
-        <translation type="unfinished"></translation>
+        <translation>تور جيىلىگى</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="619"/>
         <source>Net card</source>
-        <translation type="unfinished"></translation>
+        <translation>ورتاق ٴلاززاتتانۋ بولاتٸن تور كارتاسى</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="742"/>
         <location filename="../mobilehotspotwidget.cpp" line="750"/>
         <source>hotspot already open</source>
-        <translation type="unfinished"></translation>
+        <translation>اعٸم نۇكتەسى الدەقاشان ئېچىۋېتىلدى</translation>
     </message>
 </context>
 </TS>

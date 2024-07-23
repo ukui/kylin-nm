aab8fa56ff794cc5714e7f33acfb479785fce878
KevinDuan <duankaiwen@kylinos.cn>
2024-07-23 12:52:01 +0000

    Translated using Weblate (Kazakh)
    Currently translated at 100.0% (38 of 38 strings)
    
    Translation: openkylin-nile-new/proxy
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-nile-new/proxy/kk/

--- plugins/proxy/translations/kk.ts
+++ plugins/proxy/translations/kk.ts
@@ -6,27 +6,27 @@
     <message>
         <location filename="../aptproxydialog.cpp" line="43"/>
         <source>Set APT Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>APT ۋاكىلدىك ەتۋشى ورنالاسترعان ەتۋ</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="60"/>
         <source>Server Address</source>
-        <translation type="unfinished"></translation>
+        <translation>قىزىمەت وتەۋ اسبابى ٵدىرىسى</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="78"/>
         <source>Port</source>
-        <translation type="unfinished"></translation>
+        <translation>اۋٸزٸ</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="99"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشىنەن قالدىرۋ</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="103"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>كەسٸم جاساۋ</translation>
     </message>
 </context>
 <context>
@@ -34,27 +34,27 @@
     <message>
         <location filename="../proxy.cpp" line="63"/>
         <source>Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>ۋاكىلەتىنەن</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="201"/>
         <source>Start using</source>
-        <translation type="unfinished"></translation>
+        <translation>ٸستەتلۋٸن باستاۋ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="214"/>
         <source>Proxy mode</source>
-        <translation type="unfinished"></translation>
+        <translation>ۋاكىلەتىنەن تيپى</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="218"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation>اۆتوماتىك</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="221"/>
         <source>Manual</source>
-        <translation type="unfinished"></translation>
+        <translation>قولدانبا</translation>
     </message>
     <message>
         <source>Application Proxy</source>
@@ -63,36 +63,36 @@
     <message>
         <location filename="../proxy.cpp" line="459"/>
         <source>System Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>ۋاكىلەتىنەن سەستەما</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="461"/>
         <source>Auto url</source>
-        <translation type="unfinished"></translation>
+        <translation>URLتەڭگەرگٸش</translation>
         <extra-contents_path>/Proxy/Auto url</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="463"/>
         <source>Http Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>Http ۋاكىلەتىنەن</translation>
         <extra-contents_path>/Proxy/Http Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="465"/>
         <source>Https Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>Https ۋاكىلەتىنەن</translation>
         <extra-contents_path>/Proxy/Https Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="467"/>
         <source>Ftp Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>Ftp ۋاكىلەتىنەن</translation>
         <extra-contents_path>/Proxy/Ftp Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="469"/>
         <source>Socks Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>SOCKS ۋاكىلەتىنەن</translation>
         <extra-contents_path>/Proxy/Socks Proxy</extra-contents_path>
     </message>
     <message>
@@ -102,119 +102,119 @@
         <location filename="../proxy.cpp" line="473"/>
         <location filename="../proxy.cpp" line="1122"/>
         <source>Port</source>
-        <translation type="unfinished"></translation>
+        <translation>اۋٸزٸ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="474"/>
         <source>List of ignored hosts. more than one entry, please separate with english semicolon(;)</source>
-        <translation type="unfinished"></translation>
+        <translation>سەل قارالعان باس اپپارات تٸزٸمدٸگٸ،ەنگىلششە نۇكتەلىك پاش ٸستەتڭٸز (;)</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="477"/>
         <source>App Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>قولدانبالى ۋاكىلەتىنەن</translation>
         <extra-contents_path>/Proxy/App Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="480"/>
         <source>APT Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>APT ۋاكىلى</translation>
         <extra-contents_path>/Proxy/APT Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="481"/>
         <location filename="../proxy.cpp" line="1059"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>ٸشٸۋ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="482"/>
         <source>Server Address : </source>
-        <translation type="unfinished"></translation>
+        <translation>قىزىمەت وتەۋ اسبابى ٵدىرىسى : </translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="483"/>
         <source>Port : </source>
-        <translation type="unfinished"></translation>
+        <translation>اۋٸزٸ : </translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="484"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>تالداۋجاساۋ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="586"/>
         <source>The APT proxy has been turned off and needs to be restarted to take effect</source>
-        <translation type="unfinished"></translation>
+        <translation>APT ۆاكىلى جابٸلدٸ، قاتە قوزعالتۋعا دۇرٸس كەلسە سونان كۇشكە يە بولادٸ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="587"/>
         <location filename="../proxy.cpp" line="824"/>
         <source>Reboot Later</source>
-        <translation type="unfinished"></translation>
+        <translation>سەل تۇرٸپ قاتە قوزعالت</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="588"/>
         <location filename="../proxy.cpp" line="825"/>
         <source>Reboot Now</source>
-        <translation type="unfinished"></translation>
+        <translation>ازىر قاتە قوزعالت</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="823"/>
         <source>The system needs to be restarted to set the APT proxy, whether to reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>APT ۋاكىلدىك ەتۋشى ورنالاسترعان ەتۋ ٷشٸن سەستامانى قاتە قوزعالتۋ كەرەك، قاتە قوزغىتىلامدۇ جوق</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1071"/>
         <source>Proxy type</source>
-        <translation type="unfinished"></translation>
+        <translation>ۋاكىلەتىنەن تۇرى</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1088"/>
         <source>IP address</source>
-        <translation type="unfinished"></translation>
+        <translation>IP ٵدىرىسى</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1094"/>
         <location filename="../proxy.cpp" line="1125"/>
         <source>Required</source>
-        <translation type="unfinished"></translation>
+        <translation>ٴسوزسىز تولدٸرعٸش</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1115"/>
         <source>Invalid IP Address</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشكە يە ەمەس IP ٵدىرىسى</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1136"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>ابونىت مى</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1139"/>
         <location filename="../proxy.cpp" line="1153"/>
         <source>Optional</source>
-        <translation type="unfinished"></translation>
+        <translation>تالداپ تولدٸرعٸش</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1149"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>قۇپيا كىلت</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1172"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>كۇشىنەن قالدىرۋ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1173"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>ساقتاۋ</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1245"/>
         <source>The following applications are allowed to use this configuration:</source>
-        <translation type="unfinished"></translation>
+        <translation>تومەندەگٸ تولىقتامالاردى ٸستٸتۋگە رۇحسات قىلىندى :</translation>
     </message>
 </context>
 </TS>

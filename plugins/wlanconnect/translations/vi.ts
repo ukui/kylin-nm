e218719ea82034a44f1b30577e2636d9db203739
13977028965 <1592941704@qq.com>
2025-06-08 15:33:49 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 100.0% (11 of 11 strings)
    
    Translation: openkylin-nile-new/wlanconnect
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-nile-new/wlanconnect/vi/

--- plugins/wlanconnect/translations/vi.ts
+++ plugins/wlanconnect/translations/vi.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="../../component/AddBtn/addnetbtn.cpp" line="44"/>
         <source>Add Others</source>
-        <translation type="unfinished"></translation>
+        <translation>Tham gia vào mạng khác</translation>
     </message>
     <message>
         <location filename="../../component/AddBtn/addnetbtn.cpp" line="48"/>
         <source>Add WiredNetork</source>
-        <translation type="unfinished"></translation>
+        <translation>Thêm mạng có dây</translation>
     </message>
 </context>
 <context>
@@ -19,7 +19,7 @@
     <message>
         <location filename="../wlanconnect.ui" line="14"/>
         <source>WlanConnect</source>
-        <translation type="unfinished"></translation>
+        <translation>Mạng cục bộ không dây</translation>
     </message>
     <message>
         <location filename="../wlanconnect.ui" line="35"/>
@@ -27,20 +27,20 @@
         <location filename="../wlanconnect.cpp" line="280"/>
         <location filename="../wlanconnect.cpp" line="283"/>
         <source>WLAN</source>
-        <translation type="unfinished"></translation>
+        <translation>Mạng LAN không dây</translation>
     </message>
     <message>
         <location filename="../wlanconnect.ui" line="94"/>
         <location filename="../wlanconnect.cpp" line="282"/>
         <source>open</source>
-        <translation type="unfinished"></translation>
+        <translation>Bật</translation>
         <extra-contents_path>/wlanconnect/open</extra-contents_path>
     </message>
     <message>
         <location filename="../wlanconnect.ui" line="147"/>
         <location filename="../wlanconnect.cpp" line="279"/>
         <source>Advanced settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt nâng cao</translation>
         <extra-contents_path>/wlanconnect/Advanced settings&quot;</extra-contents_path>
     </message>
     <message>
@@ -54,29 +54,29 @@
     <message>
         <location filename="../wlanconnect.cpp" line="127"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt</translation>
     </message>
     <message>
         <location filename="../wlanconnect.cpp" line="130"/>
         <source>Settings desktop message</source>
-        <translation type="unfinished"></translation>
+        <translation>Cài đặt thông báo trên màn hình</translation>
     </message>
     <message>
         <location filename="../wlanconnect.cpp" line="298"/>
         <source>No wireless network card detected</source>
-        <translation type="unfinished"></translation>
+        <translation>Không phát hiện được card mạng không dây</translation>
     </message>
     <message>
         <location filename="../wlanconnect.cpp" line="425"/>
         <location filename="../wlanconnect.cpp" line="1104"/>
         <location filename="../wlanconnect.cpp" line="1167"/>
         <source>connected</source>
-        <translation type="unfinished"></translation>
+        <translation>Đã kết nối</translation>
     </message>
     <message>
         <location filename="../wlanconnect.cpp" line="1040"/>
         <source>card</source>
-        <translation type="unfinished"></translation>
+        <translation>Card mạng</translation>
     </message>
 </context>
 </TS>

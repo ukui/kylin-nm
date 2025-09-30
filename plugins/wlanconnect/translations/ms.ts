6c17df8d1e86e618f947df69b90d844b021d3e30
KevinDuan <duankaiwen@kylinos.cn>
2025-09-30 15:10:26 +0800

    Added translation using Weblate (Malay)

---
+++ plugins/wlanconnect/translations/ms.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ms">
+<context>
+    <name>AddNetBtn</name>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="44"/>
+        <source>Add Others</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="48"/>
+        <source>Add WiredNetwork</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WlanConnect</name>
+    <message>
+        <location filename="../wlanconnect.ui" line="14"/>
+        <source>WlanConnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.ui" line="35"/>
+        <location filename="../wlanconnect.cpp" line="141"/>
+        <location filename="../wlanconnect.cpp" line="215"/>
+        <location filename="../wlanconnect.cpp" line="217"/>
+        <source>WLAN</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/wlanconnect/WLAN</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../wlanconnect.ui" line="94"/>
+        <source>open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.ui" line="147"/>
+        <location filename="../wlanconnect.cpp" line="214"/>
+        <source>Advanced settings</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/wlanconnect/Advanced settings&quot;</extra-contents_path>
+    </message>
+    <message>
+        <source>ukui control center</source>
+        <translation type="vanished">控制面板</translation>
+    </message>
+    <message>
+        <source>ukui control center desktop message</source>
+        <translation type="vanished">控制面板桌面通知</translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="124"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="127"/>
+        <source>Settings desktop message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="212"/>
+        <source>Add Others</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/wlanconnect/Add Others&quot;</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="232"/>
+        <location filename="../wlanconnect.cpp" line="568"/>
+        <source>No wireless network card detected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="364"/>
+        <location filename="../wlanconnect.cpp" line="1057"/>
+        <location filename="../wlanconnect.cpp" line="1124"/>
+        <source>connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../wlanconnect.cpp" line="993"/>
+        <source>card</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

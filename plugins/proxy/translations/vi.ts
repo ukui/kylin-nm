54f46b6c77a0d293094b6ec059d47ea692d83b96
liweinan <liweinan@kylinos.cn>
2025-11-20 20:08:19 +0800

    手动同步冲突文件

--- plugins/proxy/translations/vi.ts
+++ plugins/proxy/translations/vi.ts
@@ -32,187 +32,188 @@
 <context>
     <name>Proxy</name>
     <message>
-        <location filename="../proxy.cpp" line="63"/>
+        <location filename="../proxy.cpp" line="68"/>
         <source>Proxy</source>
         <translation>Đại lý</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="201"/>
+        <location filename="../proxy.cpp" line="206"/>
         <source>Start using</source>
         <translation>Bắt đầu sử dụng</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="214"/>
+        <location filename="../proxy.cpp" line="219"/>
         <source>Proxy mode</source>
-        <translation>Chế độ proxy</translation>
+        <translation>Chế độ Proxy</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="218"/>
+        <location filename="../proxy.cpp" line="223"/>
         <source>Auto</source>
         <translation>Theo hệ thống</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="221"/>
+        <location filename="../proxy.cpp" line="226"/>
         <source>Manual</source>
         <translation>Sách hướng dẫn</translation>
     </message>
     <message>
+        <location filename="../proxy.cpp" line="381"/>
         <source>Application Proxy</source>
-        <translation type="vanished">应用代理</translation>
+        <translation>应用代理</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="459"/>
+        <location filename="../proxy.cpp" line="466"/>
         <source>System Proxy</source>
         <translation>Proxy hệ thống</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="461"/>
+        <location filename="../proxy.cpp" line="468"/>
         <source>Auto url</source>
-        <translation>URL tự động</translation>
+        <translation>url tự động</translation>
         <extra-contents_path>/Proxy/Auto url</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="463"/>
+        <location filename="../proxy.cpp" line="470"/>
         <source>Http Proxy</source>
-        <translation>Http Proxy</translation>
+        <translation>Http máy chủ proxy</translation>
         <extra-contents_path>/Proxy/Http Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="465"/>
+        <location filename="../proxy.cpp" line="472"/>
         <source>Https Proxy</source>
         <translation>Https Proxy</translation>
         <extra-contents_path>/Proxy/Https Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="467"/>
+        <location filename="../proxy.cpp" line="474"/>
         <source>Ftp Proxy</source>
         <translation>Ftp Proxy</translation>
         <extra-contents_path>/Proxy/Ftp Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="469"/>
+        <location filename="../proxy.cpp" line="476"/>
         <source>Socks Proxy</source>
         <translation>Socks Proxy</translation>
         <extra-contents_path>/Proxy/Socks Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="470"/>
-        <location filename="../proxy.cpp" line="471"/>
-        <location filename="../proxy.cpp" line="472"/>
-        <location filename="../proxy.cpp" line="473"/>
-        <location filename="../proxy.cpp" line="1122"/>
+        <location filename="../proxy.cpp" line="477"/>
+        <location filename="../proxy.cpp" line="478"/>
+        <location filename="../proxy.cpp" line="479"/>
+        <location filename="../proxy.cpp" line="480"/>
+        <location filename="../proxy.cpp" line="1236"/>
         <source>Port</source>
         <translation>Cổng máy chủ</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="474"/>
+        <location filename="../proxy.cpp" line="481"/>
         <source>List of ignored hosts. more than one entry, please separate with english semicolon(;)</source>
-        <translation>Danh sách các máy chủ bị bỏ qua. nhiều hơn một mục nhập, vui lòng tách bằng dấu chấm phẩy tiếng Anh (;)</translation>
+        <translation>Danh sách các máy chủ bị bỏ qua. Nếu có nhiều hơn một mục, vui lòng phân tách bằng dấu chấm phẩy (;) tiếng Anh</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="477"/>
+        <location filename="../proxy.cpp" line="484"/>
         <source>App Proxy</source>
-        <translation>Proxy ứng dụng</translation>
+        <translation>ứng dụng Proxy</translation>
         <extra-contents_path>/Proxy/App Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="480"/>
+        <location filename="../proxy.cpp" line="487"/>
         <source>APT Proxy</source>
         <translation>Proxy Apt</translation>
         <extra-contents_path>/Proxy/APT Proxy</extra-contents_path>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="481"/>
-        <location filename="../proxy.cpp" line="1059"/>
+        <location filename="../proxy.cpp" line="488"/>
+        <location filename="../proxy.cpp" line="1173"/>
         <source>Open</source>
         <translation>Bật</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="482"/>
+        <location filename="../proxy.cpp" line="489"/>
         <source>Server Address : </source>
         <translation>Địa chỉ máy chủ： </translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="483"/>
+        <location filename="../proxy.cpp" line="490"/>
         <source>Port : </source>
         <translation>Cổng： </translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="484"/>
+        <location filename="../proxy.cpp" line="491"/>
         <source>Edit</source>
         <translation>biên soạn</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="586"/>
+        <location filename="../proxy.cpp" line="646"/>
         <source>The APT proxy has been turned off and needs to be restarted to take effect</source>
-        <translation>Proxy APT đã bị tắt và cần được khởi động lại để có hiệu lực</translation>
+        <translation>Proxy APT đã bị tắt và cần phải khởi động lại để có hiệu lực</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="587"/>
-        <location filename="../proxy.cpp" line="824"/>
+        <location filename="../proxy.cpp" line="647"/>
+        <location filename="../proxy.cpp" line="935"/>
         <source>Reboot Later</source>
         <translation>Khởi động lại sau</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="588"/>
-        <location filename="../proxy.cpp" line="825"/>
+        <location filename="../proxy.cpp" line="648"/>
+        <location filename="../proxy.cpp" line="936"/>
         <source>Reboot Now</source>
         <translation>Khởi động lại ngay</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="823"/>
+        <location filename="../proxy.cpp" line="934"/>
         <source>The system needs to be restarted to set the APT proxy, whether to reboot</source>
         <translation>Việc thiết lập proxy APT cần khởi động lại hệ thống để có hiệu lực, có muốn khởi động lại hệ thống không</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1071"/>
+        <location filename="../proxy.cpp" line="1185"/>
         <source>Proxy type</source>
         <translation>Loại Proxy</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1088"/>
+        <location filename="../proxy.cpp" line="1202"/>
         <source>IP address</source>
         <translation>Địa chỉ IP</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1094"/>
-        <location filename="../proxy.cpp" line="1125"/>
+        <location filename="../proxy.cpp" line="1208"/>
+        <location filename="../proxy.cpp" line="1239"/>
         <source>Required</source>
         <translation>Bắt buộc điền</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1115"/>
+        <location filename="../proxy.cpp" line="1229"/>
         <source>Invalid IP Address</source>
         <translation>Địa chỉ IP không hợp lệ</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1136"/>
+        <location filename="../proxy.cpp" line="1250"/>
         <source>Username</source>
         <translation>Tên người dùng</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1139"/>
-        <location filename="../proxy.cpp" line="1153"/>
+        <location filename="../proxy.cpp" line="1253"/>
+        <location filename="../proxy.cpp" line="1267"/>
         <source>Optional</source>
         <translation>Tùy chọn</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1149"/>
+        <location filename="../proxy.cpp" line="1263"/>
         <source>Password</source>
         <translation>Mật khẩu</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1172"/>
+        <location filename="../proxy.cpp" line="1286"/>
         <source>Cancel</source>
         <translation>Hủy</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1173"/>
+        <location filename="../proxy.cpp" line="1287"/>
         <source>Save</source>
         <translation>Lưu</translation>
     </message>
     <message>
-        <location filename="../proxy.cpp" line="1245"/>
+        <location filename="../proxy.cpp" line="1359"/>
         <source>The following applications are allowed to use this configuration:</source>
         <translation>Cho phép ứng dụng sau sử dụng cấu hình này:</translation>
     </message>

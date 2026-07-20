6ca650fdaecef883c146805c737f4b264034a20c
Qingqing <wangyiqing@kylinos.cn>
2026-07-20 14:33:10 +0800

    Added translation using Weblate (Arabic)

---
+++ plugins/vpn/translations/ar.ts
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar" sourcelanguage="en">
+<context>
+    <name>AddNetBtn</name>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="44"/>
+        <source>Add Others</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="48"/>
+        <source>Add WiredNetork</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ItemFrame</name>
+    <message>
+        <location filename="../itemframe.cpp" line="40"/>
+        <source>Add VPN</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Vpn</name>
+    <message>
+        <location filename="../vpn.ui" line="53"/>
+        <location filename="../vpn.cpp" line="70"/>
+        <source>VPN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../vpn.ui" line="68"/>
+        <source>import</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../vpn.cpp" line="159"/>
+        <location filename="../vpn.cpp" line="291"/>
+        <source>Show on Taskbar</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Vpn/Show on Taskbar</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../vpn.cpp" line="293"/>
+        <source>Add VPN</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Vpn/Add VPN</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../vpn.cpp" line="374"/>
+        <location filename="../vpn.cpp" line="490"/>
+        <source>connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vpn.cpp" line="376"/>
+        <location filename="../vpn.cpp" line="500"/>
+        <source>not connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>VpnItem</name>
+    <message>
+        <location filename="../vpnitem.cpp" line="60"/>
+        <source>Delete</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vpnitem.cpp" line="117"/>
+        <location filename="../vpnitem.cpp" line="130"/>
+        <source>Disconnect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../vpnitem.cpp" line="119"/>
+        <location filename="../vpnitem.cpp" line="128"/>
+        <source>Connect</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

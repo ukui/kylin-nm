bd4ea0e4bfebc07ae4acfcb03a2eb78307aaef96
Qingqing <wangyiqing@kylinos.cn>
2026-07-20 14:30:40 +0800

    Added translation using Weblate (Arabic)

---
+++ plugins/mobilehotspot/translations/ar.ts
@@ -0,0 +1,207 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ar">
+<context>
+    <name>BlacklistItem</name>
+    <message>
+        <location filename="../blacklistitem.cpp" line="50"/>
+        <source>Remove</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>BlacklistPage</name>
+    <message>
+        <location filename="../blacklistpage.cpp" line="48"/>
+        <source>Blacklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConnectDevListItem</name>
+    <message>
+        <location filename="../connectdevlistitem.cpp" line="50"/>
+        <source>drag into blacklist</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConnectdevPage</name>
+    <message>
+        <location filename="../connectdevpage.cpp" line="49"/>
+        <source>Connect device</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MobileHotspot</name>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="35"/>
+        <source>MobileHotspot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="110"/>
+        <source>mobilehotspot open</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/mobilehotspot open</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="112"/>
+        <source>Mobile Hotspot</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Mobile Hotspot</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="114"/>
+        <source>Open mobile hotspot</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Open mobile hotspot</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="116"/>
+        <source>Wi-Fi Name</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Wi-Fi Name</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="118"/>
+        <source>Network Password</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Network Password</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="120"/>
+        <source>Network Frequency band</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Network Frequency band</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="122"/>
+        <source>Shared NIC port</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/Shared NIC port</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../mobilehotspot.cpp" line="108"/>
+        <source>mobilehotspot</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/mobilehotspot/mobilehotspot</extra-contents_path>
+    </message>
+</context>
+<context>
+    <name>MobileHotspotWidget</name>
+    <message>
+        <source>ukui control center</source>
+        <translation type="vanished">控制面板</translation>
+    </message>
+    <message>
+        <source>ukui control center desktop message</source>
+        <translation type="vanished">控制面板桌面通知</translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="165"/>
+        <source>wirless switch is close or no wireless device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>start to close hotspot</source>
+        <translation type="vanished">开始关闭热点</translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="190"/>
+        <source>hotpots name or device is invalid</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>can not  create hotspot with password length less than eight!</source>
+        <translation type="vanished">不能创建密码长度小于八位的热点！</translation>
+    </message>
+    <message>
+        <source>start to open hotspot </source>
+        <translation type="vanished">开始创建热点</translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="644"/>
+        <source>Contains at least 8 characters</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="245"/>
+        <source>Hotspot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="64"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="67"/>
+        <source>Settings desktop message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="1103"/>
+        <source>use </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="1104"/>
+        <source> share network, will interrupt local wireless connection</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="381"/>
+        <location filename="../mobilehotspotwidget.cpp" line="400"/>
+        <location filename="../mobilehotspotwidget.cpp" line="484"/>
+        <location filename="../mobilehotspotwidget.cpp" line="526"/>
+        <location filename="../mobilehotspotwidget.cpp" line="791"/>
+        <source>hotspot already close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="550"/>
+        <source>Open mobile hotspot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="633"/>
+        <source>Network Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="672"/>
+        <source>Network Frequency band</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="697"/>
+        <source>Shared NIC port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation type="vanished">开启</translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="611"/>
+        <source>Wi-Fi Name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="vanished">网络密码</translation>
+    </message>
+    <message>
+        <source>Frequency band</source>
+        <translation type="vanished">网络频带</translation>
+    </message>
+    <message>
+        <location filename="../mobilehotspotwidget.cpp" line="823"/>
+        <location filename="../mobilehotspotwidget.cpp" line="835"/>
+        <source>hotspot already open</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

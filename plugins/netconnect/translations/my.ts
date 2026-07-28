2942635051475c6f554414d4f8d0ba2d79d00836
Qingqing <wangyiqing@kylinos.cn>
2026-07-28 11:09:57 +0800

    Added translation using Weblate (Burmese)

---
+++ plugins/netconnect/translations/my.ts
@@ -0,0 +1,111 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="my">
+<context>
+    <name>AddNetBtn</name>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="44"/>
+        <source>Add Others</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../component/AddBtn/addnetbtn.cpp" line="48"/>
+        <source>Add WiredNetork</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LanItem</name>
+    <message>
+        <source>Delete</source>
+        <translation type="vanished">删除</translation>
+    </message>
+    <message>
+        <source>Disconnect</source>
+        <translation type="vanished">断开</translation>
+    </message>
+    <message>
+        <source>Connect</source>
+        <translation type="vanished">连接</translation>
+    </message>
+    <message>
+        <location filename="../lanitem.cpp" line="147"/>
+        <source>Connected, restricting access.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>NetConnect</name>
+    <message>
+        <location filename="../netconnect.ui" line="50"/>
+        <source>Wired Network</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.ui" line="112"/>
+        <location filename="../netconnect.cpp" line="229"/>
+        <source>open</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/netconnect/open</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../netconnect.ui" line="198"/>
+        <location filename="../netconnect.cpp" line="224"/>
+        <source>Advanced settings</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/netconnect/Advanced settings&quot;</extra-contents_path>
+    </message>
+    <message>
+        <source>ukui control center</source>
+        <translation type="vanished">控制面板</translation>
+    </message>
+    <message>
+        <source>ukui control center desktop message</source>
+        <translation type="vanished">控制面板桌面通知</translation>
+    </message>
+    <message>
+        <source>WiredConnect</source>
+        <translation type="vanished">有线网络</translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="72"/>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="75"/>
+        <source>Settings desktop message</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="92"/>
+        <location filename="../netconnect.cpp" line="227"/>
+        <location filename="../netconnect.cpp" line="230"/>
+        <source>LAN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="346"/>
+        <source>No ethernet device avaliable</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="635"/>
+        <location filename="../netconnect.cpp" line="1137"/>
+        <source>connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="637"/>
+        <location filename="../netconnect.cpp" line="952"/>
+        <location filename="../netconnect.cpp" line="1147"/>
+        <source>not connected</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../netconnect.cpp" line="714"/>
+        <source>card</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

b1264d9f1dd033118147e89956c6eae5ae5941b9
Qingqing <wangyiqing@kylinos.cn>
2026-08-20 07:24:31 +0000

    Translated using Weblate (Urdu)
    Currently translated at 100.0% (28 of 28 strings)
    
    Translation: openkylin-huanghe/mobilehotspot
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/mobilehotspot/ur/

--- plugins/mobilehotspot/translations/ur.ts
+++ plugins/mobilehotspot/translations/ur.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../blacklistitem.cpp" line="50"/>
         <source>Remove</source>
-        <translation type="unfinished"></translation>
+        <translation>خارج کریں</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../blacklistpage.cpp" line="48"/>
         <source>Blacklist</source>
-        <translation type="unfinished"></translation>
+        <translation>آلہ بلیک لسٹ</translation>
     </message>
 </context>
 <context>
@@ -22,7 +22,7 @@
     <message>
         <location filename="../connectdevlistitem.cpp" line="50"/>
         <source>drag into blacklist</source>
-        <translation type="unfinished"></translation>
+        <translation>بلیک لسٹ میں شامل کریں</translation>
     </message>
 </context>
 <context>
@@ -30,7 +30,7 @@
     <message>
         <location filename="../connectdevpage.cpp" line="49"/>
         <source>Connect device</source>
-        <translation type="unfinished"></translation>
+        <translation>منسلک آلات</translation>
     </message>
 </context>
 <context>
@@ -38,54 +38,54 @@
     <message>
         <location filename="../mobilehotspot.cpp" line="35"/>
         <source>MobileHotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ</translation>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="110"/>
         <source>mobilehotspot open</source>
-        <translation type="unfinished"></translation>
+        <translation>ہاٹ اسپاٹ کھولیں</translation>
         <extra-contents_path>/mobilehotspot/mobilehotspot open</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="112"/>
         <source>Mobile Hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ</translation>
         <extra-contents_path>/mobilehotspot/Mobile Hotspot</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="114"/>
         <source>Open mobile hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ آن کریں</translation>
         <extra-contents_path>/mobilehotspot/Open mobile hotspot</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="116"/>
         <source>Wi-Fi Name</source>
-        <translation type="unfinished"></translation>
+        <translation>وائی فائی کا نام</translation>
         <extra-contents_path>/mobilehotspot/Wi-Fi Name</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="118"/>
         <source>Network Password</source>
-        <translation type="unfinished"></translation>
+        <translation>نیٹ ورک پاس ورڈ</translation>
         <extra-contents_path>/mobilehotspot/Network Password</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="120"/>
         <source>Network Frequency band</source>
-        <translation type="unfinished"></translation>
+        <translation>نیٹ ورک بینڈ</translation>
         <extra-contents_path>/mobilehotspot/Network Frequency band</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="122"/>
         <source>Shared NIC port</source>
-        <translation type="unfinished"></translation>
+        <translation>وائی فائی کارڈ پورٹ شیئر کریں</translation>
         <extra-contents_path>/mobilehotspot/Shared NIC port</extra-contents_path>
     </message>
     <message>
         <location filename="../mobilehotspot.cpp" line="108"/>
         <source>mobilehotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ</translation>
         <extra-contents_path>/mobilehotspot/mobilehotspot</extra-contents_path>
     </message>
 </context>
@@ -102,7 +102,7 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="165"/>
         <source>wirless switch is close or no wireless device</source>
-        <translation type="unfinished"></translation>
+        <translation>وائیرلیس سوئچ آف ہے یا ایسا کوئی وائی فائی کارڈ موجود نہیں جس میں ہاٹ اسپاٹ کی سہولت ہو</translation>
     </message>
     <message>
         <source>start to close hotspot</source>
@@ -111,7 +111,7 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="190"/>
         <source>hotpots name or device is invalid</source>
-        <translation type="unfinished"></translation>
+        <translation>ہاٹ اسپاٹ کا نام یا آلہ غلط ہے</translation>
     </message>
     <message>
         <source>can not  create hotspot with password length less than eight!</source>
@@ -124,32 +124,32 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="644"/>
         <source>Contains at least 8 characters</source>
-        <translation type="unfinished"></translation>
+        <translation>کم از کم 8 حروف پر مشتمل ہو</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="245"/>
         <source>Hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="64"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation>ترتیبات</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="67"/>
         <source>Settings desktop message</source>
-        <translation type="unfinished"></translation>
+        <translation>ترتیبات ڈیسک ٹاپ اطلاعات</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="1103"/>
         <source>use </source>
-        <translation type="unfinished"></translation>
+        <translation>استعمال کریں </translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="1104"/>
         <source> share network, will interrupt local wireless connection</source>
-        <translation type="unfinished"></translation>
+        <translation> ہاٹ اسپاٹ شیئر کرنے سے آپ کا وائیرلیس کنیکشن منقطع ہو سکتا ہے</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="381"/>
@@ -158,27 +158,27 @@
         <location filename="../mobilehotspotwidget.cpp" line="526"/>
         <location filename="../mobilehotspotwidget.cpp" line="791"/>
         <source>hotspot already close</source>
-        <translation type="unfinished"></translation>
+        <translation>ہاٹ اسپاٹ آف ہے</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="550"/>
         <source>Open mobile hotspot</source>
-        <translation type="unfinished"></translation>
+        <translation>موبائل ہاٹ اسپاٹ آن کریں</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="633"/>
         <source>Network Password</source>
-        <translation type="unfinished"></translation>
+        <translation>نیٹ ورک پاس ورڈ</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="672"/>
         <source>Network Frequency band</source>
-        <translation type="unfinished"></translation>
+        <translation>نیٹ ورک بینڈ</translation>
     </message>
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="697"/>
         <source>Shared NIC port</source>
-        <translation type="unfinished"></translation>
+        <translation>وائی فائی کارڈ پورٹ شیئر کریں</translation>
     </message>
     <message>
         <source>Open</source>
@@ -187,7 +187,7 @@
     <message>
         <location filename="../mobilehotspotwidget.cpp" line="611"/>
         <source>Wi-Fi Name</source>
-        <translation type="unfinished"></translation>
+        <translation>وائی فائی کا نام</translation>
     </message>
     <message>
         <source>Password</source>
@@ -201,7 +201,7 @@
         <location filename="../mobilehotspotwidget.cpp" line="823"/>
         <location filename="../mobilehotspotwidget.cpp" line="835"/>
         <source>hotspot already open</source>
-        <translation type="unfinished"></translation>
+        <translation>ہاٹ اسپاٹ آن ہے</translation>
     </message>
 </context>
 </TS>

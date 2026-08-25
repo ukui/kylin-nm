7187b70732452e018b6b0728e740657a3faeea54
KevinDuan <duankaiwen@kylinos.cn>
2026-08-25 03:33:46 +0000

    Translated using Weblate (German)
    Currently translated at 100.0% (42 of 42 strings)
    
    Translation: openkylin-huanghe/proxy
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/proxy/de/

--- plugins/proxy/translations/de.ts
+++ plugins/proxy/translations/de.ts
@@ -19,22 +19,22 @@
     <message>
         <location filename="../aptproxydialog.cpp" line="64"/>
         <source>Http Address</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTP-Proxy</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="84"/>
         <source>Http Port</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTP-Port</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="104"/>
         <source>Https Address</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTPS-Proxy</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="123"/>
         <source>Https Port</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTPS-Port</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="144"/>
@@ -151,7 +151,7 @@
     <message>
         <location filename="../proxy.cpp" line="538"/>
         <source>Http Address : </source>
-        <translation type="unfinished"></translation>
+        <translation>HTTP-Adresse: </translation>
     </message>
     <message>
         <source>Server Address : </source>
@@ -166,7 +166,7 @@
     <message>
         <location filename="../proxy.cpp" line="540"/>
         <source>Https Address : </source>
-        <translation type="unfinished"></translation>
+        <translation>HTTPS-Adresse: </translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="542"/>

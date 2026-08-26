dfe6d764276ddb68990609f3ea85bb4fd7b291e1
13832136087 <ningxiyuyi@126.com>
2026-08-26 15:46:59 +0000

    Translated using Weblate (朝鲜语 (ko_KP))
    Currently translated at 100.0% (13 of 13 strings)
    
    Translation: openkylin-huanghe/netconnect
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/netconnect/ko_KP/

--- plugins/netconnect/translations/ko_KP.ts
+++ plugins/netconnect/translations/ko_KP.ts
@@ -11,7 +11,7 @@
     <message>
         <location filename="../../component/AddBtn/addnetbtn.cpp" line="48"/>
         <source>Add WiredNetork</source>
-        <translation>유선네트워크 추가</translation>
+        <translation>유선 네트워크 추가</translation>
     </message>
 </context>
 <context>

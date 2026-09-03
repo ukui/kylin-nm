c5b98df90b11e6c7eb2aade50969d97fecf60ae4
hyoyeong <164199371@qq.com>
2026-09-03 13:22:36 +0000

    Translated using Weblate (Vietnamese)
    Currently translated at 99.3% (155 of 156 strings)
    
    Translation: openkylin-huanghe/kylin-vpn
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/kylin-vpn/vi/

--- src-vpn/translations/kylin-vpn_vi.ts
+++ src-vpn/translations/kylin-vpn_vi.ts
@@ -620,18 +620,18 @@ Nếu sử dụng hướng của khóa, nó phải ngược với phía VPN sử
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="80"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="164"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="165"/>
         <source>IPv4</source>
-        <translation></translation>
+        <translation>IPv4</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="167"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="244"/>
         <source>IPv6</source>
-        <translation></translation>
+        <translation>IPv6</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="172"/>
@@ -770,7 +770,7 @@ Nếu sử dụng hướng của khóa, nó phải ngược với phía VPN sử
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="273"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="282"/>

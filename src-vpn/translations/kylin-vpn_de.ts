6b5faab72cca5e7a7b6229a2681b18879934d4f6
hyoyeong <164199371@qq.com>
2026-09-03 13:22:33 +0000

    Translated using Weblate (German)
    Currently translated at 99.3% (155 of 156 strings)
    
    Translation: openkylin-huanghe/kylin-vpn
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/kylin-vpn/de/

--- src-vpn/translations/kylin-vpn_de.ts
+++ src-vpn/translations/kylin-vpn_de.ts
@@ -620,18 +620,18 @@ Wenn die Schlüsselrichtung verwendet wird, muss sie der verwendeten VPN-Seite e
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="80"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="164"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="165"/>
         <source>IPv4</source>
-        <translation></translation>
+        <translation>IPv4</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="167"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="244"/>
         <source>IPv6</source>
-        <translation></translation>
+        <translation>IPv6</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="172"/>
@@ -770,7 +770,7 @@ Wenn die Schlüsselrichtung verwendet wird, muss sie der verwendeten VPN-Seite e
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="273"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="282"/>

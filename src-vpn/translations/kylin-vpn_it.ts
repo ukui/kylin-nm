4ba6be3e8e4a79c5c7c96e4b33199eff21cb0662
hyoyeong <164199371@qq.com>
2026-09-03 13:22:34 +0000

    Translated using Weblate (Italian)
    Currently translated at 100.0% (156 of 156 strings)
    
    Translation: openkylin-huanghe/kylin-vpn
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-huanghe/kylin-vpn/it/

--- src-vpn/translations/kylin-vpn_it.ts
+++ src-vpn/translations/kylin-vpn_it.ts
@@ -620,18 +620,18 @@ Se viene utilizzata una direzione della chiave, questa deve essere opposta a que
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="80"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="164"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="165"/>
         <source>IPv4</source>
-        <translation></translation>
+        <translation>IPv4</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="167"/>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="244"/>
         <source>IPv6</source>
-        <translation></translation>
+        <translation>IPv6</translation>
     </message>
     <message>
         <location filename="../frontend/vpndetails/vpndetail.cpp" line="172"/>
@@ -770,7 +770,7 @@ Se viene utilizzata una direzione della chiave, questa deve essere opposta a que
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="273"/>
         <source>VPN</source>
-        <translation></translation>
+        <translation>VPN</translation>
     </message>
     <message>
         <location filename="../frontend/single-pages/vpnpage.cpp" line="282"/>

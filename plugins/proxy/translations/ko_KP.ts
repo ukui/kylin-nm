6fc904d519bf30677a1fb0910991c10eee35b540
KevinDuan <duankaiwen@kylinos.cn>
2026-09-15 16:45:07 +0800

    Added translation using Weblate (朝鲜语 (ko_KP))

---
+++ plugins/proxy/translations/ko_KP.ts
@@ -0,0 +1,249 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ko_KP">
+<context>
+    <name>AptProxyDialog</name>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="43"/>
+        <source>Set APT Proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server Address</source>
+        <translation type="vanished">服务器地址</translation>
+    </message>
+    <message>
+        <source>Port</source>
+        <translation type="vanished">端口</translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="60"/>
+        <source>Http Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="80"/>
+        <source>Http Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="100"/>
+        <source>Https Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="119"/>
+        <source>Https Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="140"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../aptproxydialog.cpp" line="144"/>
+        <source>Confirm</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Proxy</name>
+    <message>
+        <location filename="../proxy.cpp" line="72"/>
+        <source>Proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="213"/>
+        <source>Start using</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="227"/>
+        <source>Proxy mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="232"/>
+        <source>Auto</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="236"/>
+        <source>Manual</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="391"/>
+        <source>Application Proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="509"/>
+        <source>System Proxy</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="511"/>
+        <source>Auto url</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/Auto url</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="513"/>
+        <source>Http Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/Http Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="515"/>
+        <source>Https Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/Https Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="517"/>
+        <source>Ftp Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/Ftp Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="519"/>
+        <source>Socks Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/Socks Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="520"/>
+        <location filename="../proxy.cpp" line="521"/>
+        <location filename="../proxy.cpp" line="522"/>
+        <location filename="../proxy.cpp" line="523"/>
+        <location filename="../proxy.cpp" line="1329"/>
+        <source>Port</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="524"/>
+        <source>List of ignored hosts. more than one entry, please separate with english semicolon(;)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="527"/>
+        <source>App Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/App Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="530"/>
+        <source>APT Proxy</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/Proxy/APT Proxy</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="531"/>
+        <location filename="../proxy.cpp" line="1266"/>
+        <source>Open</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="532"/>
+        <source>Http Address : </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server Address : </source>
+        <translation type="vanished">服务器地址： </translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="533"/>
+        <location filename="../proxy.cpp" line="535"/>
+        <source>Port : </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="534"/>
+        <source>Https Address : </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="536"/>
+        <source>Edit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="711"/>
+        <source>The APT proxy has been turned off and needs to be restarted to take effect</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="712"/>
+        <location filename="../proxy.cpp" line="1043"/>
+        <source>Reboot Later</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="713"/>
+        <location filename="../proxy.cpp" line="1044"/>
+        <source>Reboot Now</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1042"/>
+        <source>The system needs to be restarted to set the APT proxy, whether to reboot</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1279"/>
+        <source>Proxy type</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1296"/>
+        <source>IP address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1302"/>
+        <location filename="../proxy.cpp" line="1332"/>
+        <source>Required</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1322"/>
+        <source>Invalid IP Address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1343"/>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1346"/>
+        <location filename="../proxy.cpp" line="1360"/>
+        <source>Optional</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1356"/>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1379"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1380"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../proxy.cpp" line="1452"/>
+        <source>The following applications are allowed to use this configuration:</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>

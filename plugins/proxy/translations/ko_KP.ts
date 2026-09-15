f1e75116d4b0beade82752ea81b84cac56f330c7
Anonymous <noreply@weblate.org>
2026-09-15 09:47:30 +0000

    Translated using Weblate (朝鲜语 (ko_KP))
    Currently translated at 100.0% (42 of 42 strings)
    
    Translation: openkylin-nile-sp2/proxy
    Translate-URL: http://weblate.openkylin.top/projects/openkylin-nile-sp2/proxy/ko_KP/

--- plugins/proxy/translations/ko_KP.ts
+++ plugins/proxy/translations/ko_KP.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../aptproxydialog.cpp" line="43"/>
         <source>Set APT Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>APT 프록시 설정</translation>
     </message>
     <message>
         <source>Server Address</source>
@@ -19,32 +19,32 @@
     <message>
         <location filename="../aptproxydialog.cpp" line="60"/>
         <source>Http Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Http 프록시</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="80"/>
         <source>Http Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Http 포트</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="100"/>
         <source>Https Address</source>
-        <translation type="unfinished"></translation>
+        <translation>Https 프록시</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="119"/>
         <source>Https Port</source>
-        <translation type="unfinished"></translation>
+        <translation>Https 포트</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="140"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>닫기</translation>
     </message>
     <message>
         <location filename="../aptproxydialog.cpp" line="144"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>확인</translation>
     </message>
 </context>
 <context>
@@ -52,66 +52,66 @@
     <message>
         <location filename="../proxy.cpp" line="72"/>
         <source>Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>프록시</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="213"/>
         <source>Start using</source>
-        <translation type="unfinished"></translation>
+        <translation>활성화</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="227"/>
         <source>Proxy mode</source>
-        <translation type="unfinished"></translation>
+        <translation>프록시 류형</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="232"/>
         <source>Auto</source>
-        <translation type="unfinished"></translation>
+        <translation>체계 따라가기</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="236"/>
         <source>Manual</source>
-        <translation type="unfinished"></translation>
+        <translation>사용자 설명서</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="391"/>
         <source>Application Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>응용 프록시</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="509"/>
         <source>System Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>체계 프록시</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="511"/>
         <source>Auto url</source>
-        <translation type="unfinished"></translation>
+        <translation>URL 구성</translation>
         <extra-contents_path>/Proxy/Auto url</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="513"/>
         <source>Http Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTP 프록시</translation>
         <extra-contents_path>/Proxy/Http Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="515"/>
         <source>Https Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>HTTPS 프록시</translation>
         <extra-contents_path>/Proxy/Https Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="517"/>
         <source>Ftp Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>FTP 프록시</translation>
         <extra-contents_path>/Proxy/Ftp Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="519"/>
         <source>Socks Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>SOCKS 프록시</translation>
         <extra-contents_path>/Proxy/Socks Proxy</extra-contents_path>
     </message>
     <message>
@@ -121,35 +121,35 @@
         <location filename="../proxy.cpp" line="523"/>
         <location filename="../proxy.cpp" line="1329"/>
         <source>Port</source>
-        <translation type="unfinished"></translation>
+        <translation>포트</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="524"/>
         <source>List of ignored hosts. more than one entry, please separate with english semicolon(;)</source>
-        <translation type="unfinished"></translation>
+        <translation>무시할 주기 리스트, 영문 반두점(;)을 사용하십시오</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="527"/>
         <source>App Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>응용 프록시</translation>
         <extra-contents_path>/Proxy/App Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="530"/>
         <source>APT Proxy</source>
-        <translation type="unfinished"></translation>
+        <translation>APT 프록시</translation>
         <extra-contents_path>/Proxy/APT Proxy</extra-contents_path>
     </message>
     <message>
         <location filename="../proxy.cpp" line="531"/>
         <location filename="../proxy.cpp" line="1266"/>
         <source>Open</source>
-        <translation type="unfinished"></translation>
+        <translation>활성화</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="532"/>
         <source>Http Address : </source>
-        <translation type="unfinished"></translation>
+        <translation>Http 주소： </translation>
     </message>
     <message>
         <source>Server Address : </source>
@@ -159,91 +159,91 @@
         <location filename="../proxy.cpp" line="533"/>
         <location filename="../proxy.cpp" line="535"/>
         <source>Port : </source>
-        <translation type="unfinished"></translation>
+        <translation>포트： </translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="534"/>
         <source>Https Address : </source>
-        <translation type="unfinished"></translation>
+        <translation>Https 주소： </translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="536"/>
         <source>Edit</source>
-        <translation type="unfinished"></translation>
+        <translation>편집</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="711"/>
         <source>The APT proxy has been turned off and needs to be restarted to take effect</source>
-        <translation type="unfinished"></translation>
+        <translation>APT 프록시가 꺼졌습니다. 적용을 위해 재시작이 필요합니다</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="712"/>
         <location filename="../proxy.cpp" line="1043"/>
         <source>Reboot Later</source>
-        <translation type="unfinished"></translation>
+        <translation>나중에 재시작</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="713"/>
         <location filename="../proxy.cpp" line="1044"/>
         <source>Reboot Now</source>
-        <translation type="unfinished"></translation>
+        <translation>지금 재시작</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1042"/>
         <source>The system needs to be restarted to set the APT proxy, whether to reboot</source>
-        <translation type="unfinished"></translation>
+        <translation>APT 프록시 설정 후 체계 재시작이 필요합니다. 체계를 재시작하시겠습니까</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1279"/>
         <source>Proxy type</source>
-        <translation type="unfinished"></translation>
+        <translation>프록시 류형</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1296"/>
         <source>IP address</source>
-        <translation type="unfinished"></translation>
+        <translation>IP 주소</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1302"/>
         <location filename="../proxy.cpp" line="1332"/>
         <source>Required</source>
-        <translation type="unfinished"></translation>
+        <translation>필수 입력</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1322"/>
         <source>Invalid IP Address</source>
-        <translation type="unfinished"></translation>
+        <translation>잘못된 IP 주소</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1343"/>
         <source>Username</source>
-        <translation type="unfinished"></translation>
+        <translation>사용자 이름</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1346"/>
         <location filename="../proxy.cpp" line="1360"/>
         <source>Optional</source>
-        <translation type="unfinished"></translation>
+        <translation>옵션</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1356"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>비밀번호 변경</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1379"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>닫기</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1380"/>
         <source>Save</source>
-        <translation type="unfinished"></translation>
+        <translation>다음으로 스캔</translation>
     </message>
     <message>
         <location filename="../proxy.cpp" line="1452"/>
         <source>The following applications are allowed to use this configuration:</source>
-        <translation type="unfinished"></translation>
+        <translation>다음 응용프로그람 이 이 설정을 사용하도록 허용：</translation>
     </message>
 </context>
 </TS>
